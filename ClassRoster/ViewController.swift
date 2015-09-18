bc99a780e9b0d2bbeba43fafd3484aeb8f0c4ff0
William Berry <Berry@Williams-MacBook-Pro-2.local>
2015-09-17 19:59:06 -0700

    Initial Commit

---
+++ ClassRoster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassRoster
+//
+//  Created by William Berry on 9/17/15.
+//  Copyright (c) 2015 William Berry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
